e4d96edba61bfcca1a72acd54d14d8edba1675f8
Ilyas chaoui <144786151+ichaoui56@users.noreply.github.com>
2025-02-12 14:18:00 +0100

    initial commit by creating new spring boot projet with version 17

---
+++ src/main/java/org/filrouge/gymcommunity/GymCommunityApplication.java
@@ -0,0 +1,13 @@
+package org.filrouge.gymcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/filrouge/gymcommunity/GymCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.filrouge.gymcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
